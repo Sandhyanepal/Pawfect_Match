dcf8e91330f855395c46d53544709119e812f4f0
turkesh thresh <pandeyshushan@gmail.com>
2024-10-21 13:54:38 +0545

    donation esews:9742490964 temi haru kam lagenau last ma gara maile nai garnu paro

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+// r
+// import reportWebVitals from "./reportWebVitals";
+import MyRoutes from "./MyRoutes";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <MyRoutes />
+  </React.StrictMode>
+);
+
+reportWebVitals();
